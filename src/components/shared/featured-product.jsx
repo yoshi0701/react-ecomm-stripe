70bfb08540d36334c8dae2d4f06141fe2dab3dc1
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-01-21 21:53:55 +0900

    feat: featured-product component added

---
+++ src/components/shared/featured-product.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './featured-product.scss'
+
+const FeaturedProduct = (product) => {
+  const { title, imageUrl, price } = product;
+
+  return (
+    <div className='featured=product'>
+      <div className='featured-image'>
+        <img src={imageUrl} alt='product' />
+      </div>
+      <div className='name-price'>
+        <h3>{title}</h3>
+        <p>$ {price}</p>
+        <button className='button is-black nomad-btn'>ADD TO CART</button>
+      </div>
+    </div>
+  );
+}
+
+
+export default FeaturedProduct;
