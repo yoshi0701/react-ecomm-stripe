76609626dc4ba09888ffe5bdf171727548261143
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-01-23 22:13:40 +0900

    feat: single-product component style added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Switch, Route } from 'react-router-dom';
 import HomePage from './components/home-page';
 import NotFound from './components/not-found';
 import Shop from './components/page/shop/shop';
+import SingleProduct from './components/single-product/single-product';
 import './App.scss';
 
 
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={Shop} />
+        <Route path='/product/:id' component={SingleProduct} />
         <Route path='*' component={NotFound} />
       </Switch>
     </div>
