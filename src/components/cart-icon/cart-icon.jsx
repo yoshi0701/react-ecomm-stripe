37151a2ea3b6f68b774aaa90f79c1617f36bf5fe
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-01-24 17:35:17 +0900

    feat: cart context integrated to cart icon component

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useContext} from 'react';
 import shoppingBag from '../../assets/shopping-bag.png';
+import { CartContext } from '../../context/cart-context';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
+  const { itemCount } = useContext(CartContext);
   return (
     <div className='cart-container'>
       <img src={shoppingBag} alt='shopping-cart-icon' />
+      {
+        itemCount > 0 ? <span className='cart-count'> { itemCount } </span> : null
+      }
       <span className='cart-count'> 5 </span>
     </div>
   )
